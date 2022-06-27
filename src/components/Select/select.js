acf6fe27d5e22549647b952c677bc34a4905d835
Samuel Iatarola Pereira de Carvalho Carneiro <samuel.iatarola@compasso.com.br>
2022-06-27 11:01:40 -0300

    Style: styled and positioned date-picker

--- src/components/Select/select.js
+++ src/components/Select/select.js
@@ -1,13 +1,17 @@
 import React, { useEffect, useState } from "react";
+import styled from 'styled-components'
 
 import DatePicker from 'react-datepicker';
 import "react-datepicker/dist/react-datepicker.css";
 import moment from 'moment';
+import "./select.css"
 
-export default function TableDatePicker() {
+function TableDatePicker() {
   const [date, setDate] = useState(new Date())
 
   return (
-    <DatePicker placeholderText='yyyy/mm/dd' dateFormat="yyyy/MM/dd" selected={date} onChange={date => setDate(date)} minDate={new Date("1945-01-01")} maxDate={moment().toDate()}/>
+    <DatePicker wrapperClassName="date-picker" placeholderText='yyyy/mm/dd' dateFormat="yyyy/MM/dd" selected={date} onChange={date => setDate(date)} minDate={new Date("1945-01-01")} maxDate={moment().toDate()}/>
   )
 }
+
+export default TableDatePicker
